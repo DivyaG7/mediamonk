205ec43ee06b0ddcba544f03b26ed51671be2c45
unknown <divyaneela75@gmail.com>
2024-07-19 14:53:02 +0530

    Initial commit

--- src/components/service.jsx
+++ src/components/service.jsx
@@ -6,8 +6,8 @@ import { Carousel } from 'react-bootstrap';
 export const Service = () => {
     return (
         <div>
-            <Carousel indicators={false}>
-                <Carousel.Item style={{ backgroundColor: '#181B22' }}>
+            <Carousel indicators={false} controls={true}>
+                <Carousel.Item  style={{ backgroundColor: '#181B22' }}>
                     {/* <img
                     className="d-block w-100"
                     src={Branding}
@@ -22,7 +22,7 @@ export const Service = () => {
                                 marketing campaigns, we have you covered.</p>
                         </div>
                         <div className='col-md-4 mb-3 d-flex justify-content-center align-items-center'>
-                            <div className='row' style={{ textAlign: 'left' }}>
+                            <div className='row' >
                                 <h1 style={{ fontSize: '90px' }}>01</h1>
                                 <h2>Branding</h2>
                             </div>
@@ -30,11 +30,17 @@ export const Service = () => {
                         <div className='col-md-4 mb-3 d-flex justify-content-center align-items-center'>
                             <div className='row' style={{ color: '#CFCDCD9C', textAlign: 'left' }}>
                                 <p>Brand messaging</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Naming</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Logo Design</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Visual Identity</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Package Design</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Brand Book</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                             </div>
                         </div>
                     </div>
@@ -46,7 +52,7 @@ export const Service = () => {
                     alt="Visual-service"
                 /> */}
                     <div className='row p-5 mt-5 mb-5' style={{ backgroundColor: '#886b3c' }}>
-                        <div className='col-md-4 mb-3 d-flex justify-content-center align-items-center' style={{ flexDirection: 'column', textAlign: 'left' }}>
+                        <div className='col-md-4 mb-3 d-flex justify-content-center' style={{ flexDirection: 'column', textAlign: 'left' }}>
                             <p style={{ textAlign: 'left', color: 'white' }}>[WE TAKE CARE ABOUT]</p>
                             <p className='text-white'>Team of experts is dedicated to delivering exceptional
                                 results and helping you achieve your business goals.
@@ -54,7 +60,7 @@ export const Service = () => {
                                 marketing campaigns, we have you covered.</p>
                         </div>
                         <div className='col-md-4 mb-3 d-flex justify-content-center align-items-center'>
-                            <div className='row' style={{ textAlign: 'left' }}>
+                            <div className='row' >
                                 <h1 style={{ fontSize: '90px' }}>02</h1>
                                 <h2>Visual</h2>
                             </div>
@@ -62,11 +68,17 @@ export const Service = () => {
                         <div className='col-md-4 mb-3 d-flex justify-content-center align-items-center'>
                             <div className='row' style={{ color: '#CFCDCD9C', textAlign: 'left' }}>
                                 <p>Print Design</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Advertising Design</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Infographic Design</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Publication Design</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Package Design</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Presentation Design</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                             </div>
                         </div>
                     </div>
@@ -78,12 +90,12 @@ export const Service = () => {
                     alt="Digital-service"
                 /> */}
                     <div className='row p-5 mt-5 mb-5' style={{ backgroundColor: '#886b3c' }}>
-                        <div className='col-md-4 mb-3 d-flex justify-content-center align-items-center' style={{ flexDirection: 'column', textAlign: 'left' }}>
+                        <div className='col-md-4 mb-3 d-flex justify-content-center' style={{ flexDirection: 'column', textAlign: 'left' }}>
                             <p style={{ textAlign: 'left', color: 'white' }}>[WE TAKE CARE ABOUT]</p>
                             <p className='text-white'>Digital marketing encompasses a wide range of strategies and techniques used to promote products, services, or brands through digital channels.<br></br><br></br> </p>
                         </div>
                         <div className='col-md-4 mb-3 d-flex justify-content-center align-items-center'>
-                            <div className='row' style={{ textAlign: 'left' }}>
+                            <div className='row'>
                                 <h1 style={{ fontSize: '90px' }}>03</h1>
                                 <h2>Digital</h2>
                             </div>
@@ -91,11 +103,17 @@ export const Service = () => {
                         <div className='col-md-4 mb-3 d-flex justify-content-center align-items-center'>
                             <div className='row' style={{ color: '#CFCDCD9C', textAlign: 'left' }}>
                                 <p>Search Engine Optimization (SEO)</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Content Marketing</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Social Media Marketing</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Email Marketing</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>Email / WhatsApp / Text Marketing</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                                 <p>UI/UX - Website Development</p>
+                                <div className='mb-3 w-75' style={{borderBottom: '1px solid #CECECE29'}}></div>
                             </div>
                         </div>
                     </div>
