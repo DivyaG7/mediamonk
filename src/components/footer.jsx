d41136d97c95b6139378056eefeab469ce8c03f2
unknown <divyaneela75@gmail.com>
2024-08-19 14:16:16 +0530

    Initial commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,9 +9,9 @@ export const Footer = () => {
 
     return (
         <footer>
-            <div className='footer pt-5 pb-5 pe-2 ps-2' style={{background: '#181B22', color: 'white'}}>
+            <div className='footer pt-5 pb-5 pe-2 ps-2' style={{ background: '#181B22', color: 'white' }}>
                 <div className='footer-content'>
-                    <div className='row w-100' id='footer' style={{textAlign: 'left'}}>
+                    <div className='row w-100' id='footer' style={{ textAlign: 'left' }}>
                         <div className='col-md-3 mb-5' id='foot1'>
                             {/* <Link to='/'> */}
                             <img src={logo} alt='logo' style={{ width: '100px' }} />
@@ -20,15 +20,15 @@ export const Footer = () => {
 
                         </div>
                         <div className='col-md-3 mb-4'>
-                            <h3 style={{color: '#DDA74F'}}>Usefull links</h3>
+                            <h3 style={{ color: '#DDA74F' }}>Usefull links</h3>
                             <div className='d-flex' style={{ flexDirection: 'column' }}>
-                                <Link to="home" smooth={true} duration={500} className='mb-3' style={{textDecoration: 'none', color: 'white'}}>Home</Link>
-                                <Link to="about" smooth={true} duration={500} className='mb-3' style={{textDecoration: 'none', color: 'white'}}>About Us</Link>
-                                <Link to="services" smooth={true} duration={500} className='mb-3' style={{textDecoration: 'none', color: 'white'}}>Services</Link>
+                                <Link to="home" smooth={true} duration={500} className='mb-3' style={{ textDecoration: 'none', color: 'white' }}>Home</Link>
+                                <Link to="about" smooth={true} duration={500} className='mb-3' style={{ textDecoration: 'none', color: 'white' }}>About Us</Link>
+                                <Link to="services" smooth={true} duration={500} className='mb-3' style={{ textDecoration: 'none', color: 'white' }}>Services</Link>
                             </div>
                         </div>
                         <div className='col-md-3 mb-4'>
-                            <h3 style={{color: '#DDA74F'}}>Services</h3>
+                            <h3 style={{ color: '#DDA74F' }}>Services</h3>
                             <div className='d-flex' style={{ flexDirection: 'column' }}>
                                 <p>Web Design</p>
                                 <p>Graphic Design</p>
@@ -37,10 +37,24 @@ export const Footer = () => {
                             </div>
                         </div>
                         <div className='col-md-3 mb-4'>
-                            <h3 style={{color: '#DDA74F'}}>Contact</h3>
+                            <h3 style={{ color: '#DDA74F' }}>Contact</h3>
                             <div className='d-flex' style={{ flexDirection: 'column' }}>
                                 <p>Phone number</p>
                                 <p>E-mail</p>
+                                <div className='d-flex' style={{ gap: '10px', marginTop: '10px' }}>
+                                    {/* <a href='https://www.facebook.com' target='_blank' rel='noopener noreferrer'>
+                                        <i className='fab fa-facebook social_media'></i>
+                                    </a>
+                                    <a href='https://www.twitter.com' target='_blank' rel='noopener noreferrer'>
+                                        <i className='fab fa-twitter social_media'></i>
+                                    </a> */}
+                                    <a href='https://www.instagram.com' target='_blank' rel='noopener noreferrer'>
+                                        <i className='fab fa-instagram social_media'></i>
+                                    </a>
+                                    <a href='https://www.linkedin.com/in/media-monks-a39b30323/?trk=public-profile-join-page' target='_blank' rel='noopener noreferrer'>
+                                        <i className='fab fa-linkedin social_media'></i>
+                                    </a>
+                                </div>
                             </div>
                         </div>
 
